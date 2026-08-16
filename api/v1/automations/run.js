350d019604a744bb39b06bfa8ccc7b0060171e0f
Bolaji Akande <161527884+Olori24@users.noreply.github.com>
2026-08-16 01:07:28 +0100

    fix(automation): route production execution handler

--- api/v1/automations/run.js
+++ api/v1/automations/run.js
@@ -1,6 +1,2 @@
-const { requestHandler } = require('../../../apps/api/server');
-
-module.exports = (req, res) => {
-  req.url = '/api/v1/automations/run';
-  return requestHandler(req, res);
-};
+const productionHandler = require('./run-production');
+module.exports = productionHandler;
